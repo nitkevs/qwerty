d78f51a02d8e6b3b778ba06608b1dc8936942e29
nikevs <nitkevs@gmail.com>
2021-09-20 14:06:17 +0300

    Initial 4

--- cities/views.py
+++ cities/views.py
@@ -1,3 +1,5 @@
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse_lazy
@@ -10,6 +12,7 @@ from cities.models import City
 #     'home',
 #     'CityDetailView',
 # )
+
 
 def home(request, pk=None):
     # if pk:
@@ -33,20 +36,29 @@ def home(request, pk=None):
     context = {'page_obj': page_obj, 'form': form}
     return render(request, 'cities/home.html', context)
 
+
 class CityDetailView(DetailView):
     queryset = City.objects.all()
+    template_name = 'cities/detail.html'
 
 
-class CityCreateView(CreateView):
+class CityCreateView(SuccessMessageMixin, CreateView):
     model = City
     form_class = CityForm
     template_name = 'cities/create.html'
 
+    def get_success_message(self, cleaned_data):
+        return f'Город {cleaned_data["name"]} успешно создан.'
 
-class CityUpdateView(UpdateView):
+
+class CityUpdateView(SuccessMessageMixin, UpdateView):
     model = City
     form_class = CityForm
     template_name = 'cities/update.html'
+    # success_message = f'Город успешно отредактирован.'
+
+    def get_success_message(self, cleaned_data):
+        return f'Город {self.object.name} успешно отредактирован.'
 
 
 class CityDeleteView(DeleteView):
@@ -55,11 +67,19 @@ class CityDeleteView(DeleteView):
     success_url = reverse_lazy('cities:home')
 
     def get(self, request, *args, **kwargs):
+        messages.success(request, f'Город {City.objects.get(pk=kwargs["pk"])} успешно удалён.')
         return self.post(request, *args, **kwargs)
 
     template_name = 'cities/detail.html'
+
 
 class CityListView(ListView):
     model = City
     paginate_by = 2
     template_name = 'cities/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        form = CityForm()
+        context['form'] = form
+        return context
